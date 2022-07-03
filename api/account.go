32c59622ba859c97e382fde5698abe854f8ccab6
M1racle-Heen <190109003@stu.sdu.edu.kz>
2022-07-03 12:19:18 +0600

    +User table and little changes

--- api/account.go
+++ api/account.go
@@ -7,6 +7,7 @@ import (
 
 	db "github.com/M1racle-Heen/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
 
@@ -30,6 +31,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
