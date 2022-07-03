dd06b989a599d9082b33b4199006add895c9c501
M1racle-Heen <190109003@stu.sdu.edu.kz>
2022-07-03 09:49:41 +0600

    +transfer method

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/M1racle-Heen/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -14,12 +16,17 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
 	router.DELETE("/accounts/:id", server.deleteAccount)
 	router.PUT("/accounts", server.updateAccount)
 
+	router.POST("/transfers", server.createTransfer)
 	server.router = router
 	return server
 }
